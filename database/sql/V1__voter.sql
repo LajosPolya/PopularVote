c139c3aac4f09ab33f5ba7cbaf045ac7a027fe15
LajosPolya <lajospolya94@gmail.com>
2024-10-21 22:47:43 -0400

    Add table descriptions

--- database/sql/V1__voter.sql
+++ database/sql/V1__voter.sql
@@ -1,3 +1,4 @@
+/* A citizen, someone who is legally able to vote within the region they represent */
 create table citizen (
     id bigint not null auto_increment,
     given_name varchar(128) not null,
@@ -6,12 +7,14 @@ create table citizen (
     primary key (id)
 );
 
+/* A legal policy brought forth by a politician. Citizens vote on policies. */
 create table policy (
     id bigint not null auto_increment,
     description text not null,
     primary key (id)
 );
 
+/* An opinion of a policy. This provides a way to display opinions of policies in this app. */
 create table opinion(
     id bigint not null auto_increment,
     -- TODO: should this be political_affiliation instead?
@@ -23,6 +26,7 @@ create table opinion(
     foreign key fk_policy (policy_id) references policy (id)
 );
 
+/* An anonymous vote cast by a citizen */
 create table vote(
     citizen_id bigint not null,
     policy_id bigint not null,
@@ -31,6 +35,7 @@ create table vote(
     foreign key fk_policy (policy_id) references policy (id)
 );
 
+/* A poll is written in conjunction with a vote. The poll represents whether the vote was for or against a policy */
 create table poll(
     policy_id bigint not null,
     selection enum('approve', 'disapprove', 'abstain') not null,
