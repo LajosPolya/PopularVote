b4aac7ed68f6e6a9cb0733bfb338ea64bdff7972
LajosPolya <lajospolya94@gmail.com>
2024-10-23 18:55:01 -0400

    Add error handling to cast vote sproc

--- database/sql/V2__cast_vote.sql
+++ database/sql/V2__cast_vote.sql
@@ -1,7 +1,20 @@
-/* Casts an anonymous vote on behalf of a user, on a policy. */
+/* Casts an anonymous vote on behalf of a user, on a policy.
+How to call the stored procedure:
+call cast_vote(1, 1, 1, @errno, @msg);
+select @errno, @msg;
+*/
 
-create procedure cast_vote (citizen_id bigint, policy_id bigint, selection_id int)
+create procedure cast_vote (citizen_id bigint, policy_id bigint, selection_id int, out error int, out msg text)
 begin
+
+    declare exit handler for sqlexception
+    begin
+        get diagnostics condition 1 error = mysql_errno, msg = message_text;
+        rollback;
+    end;
+
+    start transaction ;
     insert into vote (citizen_id, policy_id) values (citizen_id, policy_id);
     insert into poll (policy_id, selection_id) values (policy_id, selection_id);
-end
+    commit;
+end;
