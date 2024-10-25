c1463814213c66c4a31ecf66604b2da675cf6f4f
LajosPolya <lajospolya94@gmail.com>
2024-10-24 22:09:01 -0400

    remove comment

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	runtimeOnly("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
-	// runtimeOnly("com.mysql:mysql-connector-j")
 	runtimeOnly("io.asyncer:r2dbc-mysql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
