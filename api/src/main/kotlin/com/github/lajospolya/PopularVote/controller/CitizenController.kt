49f8e65ee288dcb99c68edbda511b5763355530e
LajosPolya <lajospolya94@gmail.com>
2024-10-26 14:52:38 -0400

    only allow GET method for citizens and citizens/{id} resources

--- api/src/main/kotlin/com/github/lajospolya/PopularVote/controller/CitizenController.kt
+++ api/src/main/kotlin/com/github/lajospolya/PopularVote/controller/CitizenController.kt
@@ -4,6 +4,7 @@ import com.github.lajospolya.PopularVote.dto.CitizenDto
 import com.github.lajospolya.PopularVote.service.CitizenService
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -13,12 +14,12 @@ import kotlin.coroutines.RestrictsSuspension
 class CitizenController(
     val citizenService: CitizenService,
 ) {
-    @RequestMapping("citizen")
+    @RequestMapping("citizen", method = [RequestMethod.GET])
     fun getCitizens(): Flux<CitizenDto> {
         return citizenService.getCitizens()
     }
 
-    @RequestMapping("citizen/{id}")
+    @RequestMapping("citizen/{id}", method = [RequestMethod.GET])
     fun getCitizen(@PathVariable id: Long): Mono<CitizenDto> {
         return citizenService.getCitizen(id)
     }
