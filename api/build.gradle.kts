6f30f613e21b3629281c121c29d199af9ded7ce0
LajosPolya <lajospolya94@gmail.com>
2024-11-05 18:55:09 -0500

    add ktlint

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
 	id ("org.jetbrains.kotlin.kapt")  version "2.0.21"
+	id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "com.github.lajospolya"
