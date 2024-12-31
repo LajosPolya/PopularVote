d0cbde111927fc13d5ead4a1b36415af9320dd40
LajosPolya <lajospolya94@gmail.com>
2024-12-31 00:02:11 -0500

    deploy Popular Vote API as AWS ECS Fargate service

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -5,6 +5,8 @@ import (
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsecr"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsecs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecspatterns"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awselasticloadbalancingv2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslogs"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsrds"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -20,7 +22,13 @@ type DeployDatabaseStackProps struct {
 	awsec2.IVpc
 }
 
-func NewDatabaseStack(scope constructs.Construct, id string, props *DeployDatabaseStackProps) awscdk.Stack {
+type DeployDatabaseStack struct {
+	awscdk.Stack
+	awsrds.DatabaseCluster
+	awsecs.Cluster
+}
+
+func NewDatabaseStack(scope constructs.Construct, id string, props *DeployDatabaseStackProps) DeployDatabaseStack {
 	var sprops awscdk.StackProps
 	var vpc awsec2.IVpc
 	if props != nil {
@@ -43,11 +51,13 @@ func NewDatabaseStack(scope constructs.Construct, id string, props *DeployDataba
 	s := make([]awsec2.ISecurityGroup, 1)
 	s[0] = sg
 	// add security group rule to allow inbound traffic
+
+	defaultDatabaseName := "popularVote"
 	dbCluster := awsrds.NewDatabaseCluster(stack, jsii.String("popularVoteDbCluster"), &awsrds.DatabaseClusterProps{
 		Engine: awsrds.DatabaseClusterEngine_AuroraMysql(&awsrds.AuroraMysqlClusterEngineProps{
 			Version: awsrds.AuroraMysqlEngineVersion_VER_3_07_1(),
 		}),
-		DefaultDatabaseName: jsii.String("popularVote"),
+		DefaultDatabaseName: &defaultDatabaseName,
 		DeletionProtection:  jsii.Bool(false),
 		RemovalPolicy:       awscdk.RemovalPolicy_DESTROY,
 		SecurityGroups:      &s,
@@ -66,7 +76,7 @@ func NewDatabaseStack(scope constructs.Construct, id string, props *DeployDataba
 		Value: dbCluster.Secret().SecretFullArn(),
 	})
 
-	awsecs.NewCluster(stack, jsii.String("popularVoteCluster"), &awsecs.ClusterProps{
+	cluster := awsecs.NewCluster(stack, jsii.String("popularVoteCluster"), &awsecs.ClusterProps{
 		ClusterName: jsii.String("popularVoteCluster"),
 		Vpc:         vpc,
 	})
@@ -77,33 +87,38 @@ func NewDatabaseStack(scope constructs.Construct, id string, props *DeployDataba
 		MemoryMiB:     jsii.String("512"),
 	})
 
-	containerEnv := make(map[string]*string)
-	containerEnv["FLYWAY_USER"] = dbCluster.Secret().SecretValueFromJson(jsii.String("username")).UnsafeUnwrap()
-	containerEnv["FLYWAY_PASSWORD"] = dbCluster.Secret().SecretValueFromJson(jsii.String("password")).UnsafeUnwrap()
-	url := "jdbc:mysql://" + *(dbCluster.ClusterEndpoint().Hostname()) + ":3306/popularVote?allowPublicKeyRetrieval=true"
-	containerEnv["FLYWAY_URL"] = &url
+	// building the URL shouldn't be responsibility of the deployer. Maybe this should be done in the dockerfile?
+	url := "jdbc:mysql://" + *(dbCluster.ClusterEndpoint().Hostname()) + ":3306?allowPublicKeyRetrieval=true"
 	taskDefinition.AddContainer(jsii.String("popularVoteMigrationContainer"), &awsecs.ContainerDefinitionOptions{
-		Image:       awsecs.ContainerImage_FromAsset(jsii.String("../database/"), &awsecs.AssetImageProps{}),
-		Environment: &containerEnv,
+		Image: awsecs.ContainerImage_FromAsset(jsii.String("../database/"), &awsecs.AssetImageProps{}),
+		Environment: &map[string]*string{
+			"FLYWAY_SCHEMAS": &defaultDatabaseName,
+			"FLYWAY_URL":     &url,
+		},
 		Logging: awsecs.LogDrivers_AwsLogs(&awsecs.AwsLogDriverProps{
 			StreamPrefix:  jsii.String("popularVoteMigrationLogs"),
 			Mode:          awsecs.AwsLogDriverMode_NON_BLOCKING,
 			MaxBufferSize: awscdk.Size_Mebibytes(jsii.Number(25)),
 			LogRetention:  awslogs.RetentionDays_ONE_DAY,
 		}),
+		Secrets: &map[string]awsecs.Secret{
+			"FLYWAY_USER":     awsecs.Secret_FromSecretsManager(dbCluster.Secret(), jsii.String("username")),
+			"FLYWAY_PASSWORD": awsecs.Secret_FromSecretsManager(dbCluster.Secret(), jsii.String("password")),
+		},
 	})
 
 	awscdk.NewCfnOutput(stack, jsii.String("taskDefinitionArn"), &awscdk.CfnOutputProps{
 		Value: taskDefinition.TaskDefinitionArn(),
 	})
 
-	awscdk.NewCfnOutput(stack, jsii.String("secretValue"), &awscdk.CfnOutputProps{
-		Value: dbCluster.Secret().SecretValueFromJson(jsii.String("password")).UnsafeUnwrap(),
-	})
+	//  aws ecs run-task --task-definition DeployDatabaseStackpopularVoteDbMigrationTaskF0B0DA1D --cluster popularVoteCluster --network-configuration "awsvpcConfiguration={subnets=['subnet-0bdbf708446037956'],securityGroups=['sg-0d734f1e8f7e0b791'],assignPublicIp=ENABLED}" --launch-type FARGATE
 
-	//   aws ecs run-task --task-definition DeployDatabaseStackpopularVoteDbMigrationTaskF0B0DA1D --cluster popularVoteCluster --network-configuration "awsvpcConfiguration={subnets=['subnet-0ebfd503a09beeafb'],securityGroups=['sg-081ec50683589e6ea']}" --launch-type FARGATE
-
-	return stack
+	return DeployDatabaseStack{
+		stack,
+		dbCluster,
+		// Move this to Foundation stack?
+		cluster,
+	}
 }
 
 type DeployFoundationStack struct {
@@ -156,6 +171,114 @@ func NewFoundationStack(scope constructs.Construct, id string, props *DeployStac
 	}
 }
 
+type DeployApplicationStackProps struct {
+	awscdk.StackProps
+	awsec2.IVpc
+	awsecs.Cluster
+	awsrds.DatabaseCluster
+}
+
+func NewApplicationStack(scope constructs.Construct, id string, props *DeployApplicationStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	var vpc awsec2.IVpc
+	var cluster awsecs.Cluster
+	var dbCluster awsrds.DatabaseCluster
+	if props != nil {
+		sprops = props.StackProps
+		vpc = props.IVpc
+		cluster = props.Cluster
+		dbCluster = props.DatabaseCluster
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	/* NEVER DO THIS!
+	The database should live in a Private Subnet to prevent unauthorized access from the public
+	internet. This database is deployed to a public subnet so I can run migrations on it from my
+	local machine but I'll need to update this in the future to improve security measures.
+	*/
+	sg := awsec2.NewSecurityGroup(stack, jsii.String("popularVoteAppSg"), &awsec2.SecurityGroupProps{
+		Vpc:               vpc,
+		SecurityGroupName: jsii.String("popularVoteAppSg"),
+	})
+
+	sg.AddIngressRule(awsec2.Peer_AnyIpv4(), awsec2.Port_AllTcp(), jsii.String("allInboundTcp"), jsii.Bool(false))
+	s := make([]awsec2.ISecurityGroup, 1)
+	s[0] = sg
+
+	taskDefinition := awsecs.NewFargateTaskDefinition(stack, jsii.String("popularVoteAppTask"), &awsecs.FargateTaskDefinitionProps{
+		Cpu:            jsii.Number(256),
+		MemoryLimitMiB: jsii.Number(512),
+	})
+
+	// taskDefinition := awsecs.NewTaskDefinition(stack, jsii.String("popularVoteAppTask"), &awsecs.TaskDefinitionProps{
+	// 	Cpu:           jsii.String("256"),
+	// 	Compatibility: awsecs.Compatibility_FARGATE,
+	// 	MemoryMiB:     jsii.String("512"),
+	// })
+
+	url := "r2dbc:mysql://" + *(dbCluster.ClusterEndpoint().Hostname()) + ":3306/popularVote?allowPublicKeyRetrieval=true"
+	taskDefinition.AddContainer(jsii.String("popularVoteAppContainer"), &awsecs.ContainerDefinitionOptions{
+		Image: awsecs.ContainerImage_FromAsset(jsii.String("../api/"), &awsecs.AssetImageProps{}),
+		Environment: &map[string]*string{
+			"SPRING_R2DBC_URL": &url,
+		},
+		Logging: awsecs.LogDrivers_AwsLogs(&awsecs.AwsLogDriverProps{
+			StreamPrefix:  jsii.String("popularVoteAppLogs"),
+			Mode:          awsecs.AwsLogDriverMode_NON_BLOCKING,
+			MaxBufferSize: awscdk.Size_Mebibytes(jsii.Number(25)),
+			LogRetention:  awslogs.RetentionDays_ONE_DAY,
+		}),
+		PortMappings: &[]*awsecs.PortMapping{{
+			ContainerPort: jsii.Number(8080),
+			AppProtocol:   awsecs.AppProtocol_Http(),
+			// HostPort:      jsii.Number(80),
+			Name: jsii.String("app_mapping"),
+		}},
+		Secrets: &map[string]awsecs.Secret{
+			"SPRING_R2DBC_USERNAME": awsecs.Secret_FromSecretsManager(dbCluster.Secret(), jsii.String("username")),
+			"SPRING_R2DBC_PASSWORD": awsecs.Secret_FromSecretsManager(dbCluster.Secret(), jsii.String("password")),
+		},
+	})
+
+	service := awsecspatterns.NewApplicationLoadBalancedFargateService(stack, jsii.String("popularVoteApp"), &awsecspatterns.ApplicationLoadBalancedFargateServiceProps{
+		Cluster:        cluster,
+		DesiredCount:   jsii.Number(1),
+		ServiceName:    jsii.String("popularVoteApi"),
+		TaskDefinition: taskDefinition,
+		AssignPublicIp: jsii.Bool(true),
+		SecurityGroups: &s,
+		// Vpc:            vpc,
+		TaskSubnets: &awsec2.SubnetSelection{
+			SubnetType: awsec2.SubnetType_PUBLIC,
+		},
+	})
+
+	service.TargetGroup().ConfigureHealthCheck(&awselasticloadbalancingv2.HealthCheck{
+		HealthyHttpCodes: jsii.String("204"),
+		Path:             jsii.String("/health"),
+	})
+
+	// awsecs.NewFargateService(stack, jsii.String("popularVoteApp"), &awsecs.FargateServiceProps{
+	// 	Cluster:        cluster,
+	// 	DesiredCount:   jsii.Number(1),
+	// 	ServiceName:    jsii.String("popularVoteApi"),
+	// 	TaskDefinition: taskDefinition,
+	// 	AssignPublicIp: jsii.Bool(true),
+	// 	SecurityGroups: &s,
+	// 	VpcSubnets: &awsec2.SubnetSelection{
+	// 		SubnetType: awsec2.SubnetType_PUBLIC,
+	// 	},
+	// })
+
+	awscdk.NewCfnOutput(stack, jsii.String("appTaskDefinitionArn"), &awscdk.CfnOutputProps{
+		Value: taskDefinition.TaskDefinitionArn(),
+	})
+
+	//  aws ecs run-task --task-definition DeployDatabaseStackpopularVoteDbMigrationTaskF0B0DA1D --cluster popularVoteCluster --network-configuration "awsvpcConfiguration={subnets=['subnet-0bdbf708446037956'],securityGroups=['sg-0d734f1e8f7e0b791'],assignPublicIp=ENABLED}" --launch-type FARGATE
+
+	return stack
+}
+
 func main() {
 	defer jsii.Close()
 
@@ -167,11 +290,20 @@ func main() {
 		},
 	})
 
-	NewDatabaseStack(app, "DeployDatabaseStack", &DeployDatabaseStackProps{
+	databaseStack := NewDatabaseStack(app, "DeployDatabaseStack", &DeployDatabaseStackProps{
 		awscdk.StackProps{
 			Env: env(),
 		},
 		foundationStack.IVpc,
+	})
+
+	NewApplicationStack(app, "DeployApplicationStack", &DeployApplicationStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+		foundationStack.IVpc,
+		databaseStack.Cluster,
+		databaseStack.DatabaseCluster,
 	})
 
 	app.Synth(nil)
