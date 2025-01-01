a3f7ba2f9cfbdb0a62d4ad056d407e962ed9ace1
LajosPolya <lajospolya94@gmail.com>
2025-01-01 17:54:28 -0500

    Add health controller

---
+++ api/src/main/kotlin/com/github/lajospolya/popularVote/controller/HealthController.kt
@@ -0,0 +1,15 @@
+package com.github.lajospolya.popularVote.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    @RequestMapping("health", method = [RequestMethod.GET])
+    fun health(): ResponseEntity<Void> {
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+}
