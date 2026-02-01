7149b797ca6b0d456733f3da302382634ad90d88
Lajos Polya <lajospolya94@gmail.com>
2026-01-31 22:14:01 -0500

    Integration test to create and fetch Citizen

---
+++ api/src/test/kotlin/com/github/lajospolya/popularVote/controller/CitizenControllerIntegrationTest.kt
@@ -0,0 +1,63 @@
+package com.github.lajospolya.popularVote.controller
+
+import com.github.lajospolya.popularVote.dto.CitizenDto
+import com.github.lajospolya.popularVote.dto.CreateCitizenDto
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class CitizenControllerIntegrationTest {
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun `create citizen and then fetch it`() {
+        val createCitizenDto =
+            CreateCitizenDto(
+                givenName = "John",
+                surname = "Doe",
+                middleName = "Quincy",
+            )
+
+        val createdCitizen =
+            webTestClient
+                .post()
+                .uri("/citizens")
+                .bodyValue(createCitizenDto)
+                .exchange()
+                .expectStatus()
+                .isOk
+                .expectBody(CitizenDto::class.java)
+                .returnResult()
+                .responseBody
+
+        assertNotNull(createdCitizen)
+        assertNotNull(createdCitizen?.id)
+        assertEquals(createCitizenDto.givenName, createdCitizen?.givenName)
+        assertEquals(createCitizenDto.surname, createdCitizen?.surname)
+        assertEquals(createCitizenDto.middleName, createdCitizen?.middleName)
+
+        val fetchedCitizen =
+            webTestClient
+                .get()
+                .uri("/citizens/${createdCitizen?.id}")
+                .exchange()
+                .expectStatus()
+                .isOk
+                .expectBody(CitizenDto::class.java)
+                .returnResult()
+                .responseBody
+
+        assertNotNull(fetchedCitizen)
+        assertEquals(createdCitizen?.id, fetchedCitizen?.id)
+        assertEquals(createCitizenDto.givenName, fetchedCitizen?.givenName)
+        assertEquals(createCitizenDto.surname, fetchedCitizen?.surname)
+        assertEquals(createCitizenDto.middleName, fetchedCitizen?.middleName)
+    }
+}
