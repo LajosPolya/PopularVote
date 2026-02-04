64fd41dd01e1bc61a3542b8c7909cc21adc6d7a7
Lajos Polya <lajospolya94@gmail.com>
2026-02-03 22:22:48 -0500

    send scopes from UI to API

--- ui/src/index.js
+++ ui/src/index.js
@@ -18,6 +18,7 @@ root.render(
       authorizationParams={{
         redirect_uri: window.location.origin,
         audience: audience,
+        scope: "openid profile email read:policies",
       }}
     >
       <App />
