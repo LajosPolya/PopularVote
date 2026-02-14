47c30eb0684bd151e9ab9d99e5860ef53c532fbc
Lajos Polya <lajospolya94@gmail.com>
2026-02-14 13:35:42 -0500

    update UI with Level of Government details

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -22,6 +22,8 @@ export interface Policy {
     id: number;
     description: string;
     publisherCitizenId: number;
+    levelOfPoliticsId: number;
+    citizenPoliticalDetailsId: number;
     publisherName: string;
     isBookmarked: boolean;
 }
@@ -36,8 +38,6 @@ export interface OpinionDetails {
 }
 
 export interface PolicyDetails extends Policy {
-    publisherCitizenId: number;
-    publisherName: string;
     publisherPoliticalAffiliation: string;
     coAuthorCitizens: Citizen[];
     opinions: OpinionDetails[];
